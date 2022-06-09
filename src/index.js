a9eb3c10b9920e877388a46216b383984d65b42b
Jennifer Gifford <jgifford82@gmail.com>
2022-06-08 20:32:26 -0600

    commit tree 06e3f3a6c5e09d2f553f9fcff13600440ef91a1d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+       event.preventDefault();
+       const input = document.querySelector('input#searchByID');
+    //   This also works to grab the user input:
+    //  console.log(event.target.children[1].value); 
+        console.log(input.value);
+        fetch('http://localhost:3000/movies')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        });
+    }); 
 }
 
+
+
+
 document.addEventListener('DOMContentLoaded', init);
+// console.log('DOMContentLoaded');
